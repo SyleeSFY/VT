bcd6dd22eb16abc1487a73b0296745de4b5c980f
Artem Ryklin <sylees@yandex.ru>
2025-06-20 23:07:51 +0300

    Создание NavBar

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import Nav from '../nav/nav'
 const Home =()=>{
     return ( <div>
+      <Nav />
       <h1>Добро пожаловать на главную страницу!</h1>
       <p>Это домашняя страница вашего приложения.</p>
     </div>)
---
+++ src/pages/nav/nav.jsx
@@ -0,0 +1,7 @@
+import './nav.css'; // Путь относительно расположения файла
+const Nav = () => {
+    return(
+        <div className='NavBar'>123</div>
+    )
+}
+export default Nav;
