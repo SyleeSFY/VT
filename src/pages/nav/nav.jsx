0d2036caffd06972bc119d2ec495a21b1c9c82f7
Artem Ryklin <sylees@yandex.ru>
2025-06-22 02:45:02 +0300

    test

--- src/pages/nav/nav.jsx
+++ src/pages/nav/nav.jsx
@@ -1,20 +1,43 @@
 import './nav.css';
-import NavBarImage from './NavBarIMG.png'; // Импортируем изображение
+import NavBarImage from './NavBarIMG.png';
 
 const Nav = () => {
     return (
-        <div className='NavBar'>
+        <nav className='NavBar'>
             <img src={NavBarImage} alt="Navigation Bar" className='NavBarIMG' />
             <div className='NavMenu'>
-                <ul>
-                <li>О КАФЕДРЕ</li>
-                <li>АБИТУРЕНТАМ</li>
-                <li>СТУДЕНТАМ</li>
-                <li>КОНТАКТЫ</li>
+                <ul className='Navli'>
+                    <li>
+                        О КАФЕДРЕ
+                        <ul className='PodL'>
+                            <li>Подпункт 1</li>
+                            <li>Подпункт 2</li>
+                        </ul>
+                    </li>
+                    <li>
+                        АБИТУРЕНТАМ
+                        <ul className='PodL'>
+                            <li>Подпункт 1</li>
+                            <li>Подпункт 2</li>
+                        </ul>
+                    </li>
+                    <li>
+                        СТУДЕНТАМ
+                        <ul className='PodL'>
+                            <li>Подпункт 1</li>
+                            <li>Подпункт 2</li>
+                        </ul>
+                    </li>
+                    <li>
+                        КОНТАКТЫ
+                        <ul className='PodL'>
+                            <li>Подпункт 1</li>
+                            <li>Подпункт 2</li>
+                        </ul>
+                    </li>
                 </ul>
-
             </div>
-        </div>
+        </nav>
     )
 }
 export default Nav;
