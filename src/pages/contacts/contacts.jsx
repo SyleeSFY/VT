9b199e2e2a7a72f2c2cce95dd16185ae85378e18
Artem Ryklin <sylees@yandex.ru>
2025-06-27 02:18:29 +0300

    test

--- src/pages/contacts/contacts.jsx
+++ src/pages/contacts/contacts.jsx
@@ -1,54 +1,41 @@
 import React from 'react';
-import '../../styles/home.css';
+import '../../styles/contacts.css';
 import OurGraduatesInfoIMG from '../../components/Icons/VT.png';
 
 const Contacts = () => {
-  return (<div className='home'>
-    <div className="FlexStart">
-      <div className="BlockTittleHome">
-        <span className='VT'>Кафедра вычислительной техники</span>
-      </div>
-    </div>
-    <div className="InfoCafedra">
+  return (
+    <div className='home'>
 
-      <div className="CafedraCards">
-
-        <div className="CafedraCard">
-          <div className="Stick"></div>
-          <p>Кафедра <span style={{ color: '#00471bd8' }}>вычислительной техники </span>- одна из старейших кафедр в Смоленском филиале НИУ "МЭИ".
-            <br />Кафедра выпускает высококвалифицированных специалистов в области информационных технологий и вычислительной техники.
-
-
-          </p>
-        </div>
-        <div className="CafedraCard">
-          <div className="Stick"></div>
-          <p>Подготовка студентов кафедры вычислительной техники ведется опытным профессорско-преподавательским составом в количестве 23-х человек, из которых 4 доктора наук и 16 кандидатов наук.
-          </p>
-        </div>
-        <div className="CafedraCard">
-          <div className="Stick"></div>
-          <p>Обучение студентов происходит по направлению <span style={{ color: '#00471bd8' }}>09.03.01 - "ИНФОРМАТИКА И ВЫЧИСЛИТЕЛЬНАЯ ТЕХНИКА"</span>.
-          </p>
-        </div>
-      </div>
-      <div className="Stip" style={{ marginRight: 0 }}>
-      </div>
-
-
-    </div>
-    <div className="DivInfo">
-      <div className="FlexEnd">
+      <div className="FlexEnd" style={{ paddingRight: 60 }}>
         <div className="BlockTittleHome">
-          <span className='TittleHome'>Наши выпускники работают</span>
-          <div className="Stip" style={{ width: "100%" }}></div>
+          <span className='VT'>Контакты</span>
+
+        </div>
+      </div>
+      <div className="Block">
+        <div className="FlexCenter">
+          <div className="BlockTittleHome">
+            <span className='TittleHome' style={{ width: "80%" }}>Контакты для вашего удобства!</span>
+            <div className="Stip" style={{ width: "80%" }}></div>
+          </div>
+        </div>
+        <div className="BlockContacts">
+          <div className="LeftBlockContacts">
+            <div className="Left">
+              <div className="FirstBlock">1</div>
+              <div className="SecondBlock">2</div>
+            </div>
+            <div className="Right">
+              <div className="FirstBlock">1</div>
+              <div className="SecondBlock">2</div>
+            </div>
+          </div>
+          <div className="RightBlockContacts">
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A15441288ef2d97f04fc48793859714b354c70979f4e67308bb0cdf10ecdaeeb8&amp;source=constructor" width="690" height="500" frameborder="0"></iframe>
+          </div>
         </div>
       </div>
 
-     
-
-
-      </div>
 
     </div>
 
