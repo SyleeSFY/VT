bc4633b2a410f654e343bc7da9b6ac8bcfb655d6
Artem Ryklin <sylees@yandex.ru>
2025-07-06 14:03:31 +0300

    home page mobile

--- src/pages/nav/nav.jsx
+++ src/pages/nav/nav.jsx
@@ -1,37 +1,35 @@
 import '../../styles/nav.css';
 import { Link } from 'react-router-dom';
 import NavBarImage from '../../components/Icons/NavBarIMG.png';
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Nav = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <nav className='NavBar'>
             <img src={NavBarImage} alt="Логотип кафедры" className='NavBarIMG' />
-            <nav className='NavMenu'>
+            
+
+            <button className="mobile-menu-button" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                {isMenuOpen ? <FaTimes /> : <FaBars />}
+            </button>
+
+            <nav className={`NavMenu ${isMenuOpen ? 'mobile-open' : ''}`}>
                 <ul className='Navli'>
-
-                    <Link to="/" className="NavLink"> <li className='NavItem Cursor'>О КАФЕДРЕ</li></Link>
-                    <Link to="/educator" className="NavLink"> <li className='NavItem Cursor'>ПРЕПОДАВАТЕЛИ И СОТРУДНИКИ</li></Link>
-                    <li className='NavItem NonCursor'>
-                        АБИТУРЕНТАМ
-                        <ul className='secondNav Cursor'>
-                            <li>Подпункт 1</li>
-                            <li>Подпункт 2</li>
-                        </ul>
-                    </li>
-                    <li className='NavItem NonCursor'>
-                        СТУДЕНТАМ
-                        <ul className='secondNav Cursor'>
-                            <li>Подпункт 1</li>
-                            <li>Подпункт 2</li>
-                        </ul>
-                    </li>
-
-
-                    <Link to="/contacts" className="NavLink"> <li className='NavItem Cursor'>КОНТАКТЫ</li></Link>
-
+                    <Link to="/" className="NavLink"> 
+                        <li className='NavItem Cursor'>О КАФЕДРЕ</li>
+                    </Link>
+                    <Link to="/educator" className="NavLink"> 
+                        <li className='NavItem Cursor'>ПРЕПОДАВАТЕЛИ И СОТРУДНИКИ</li>
+                    </Link>
+                    <Link to="/contacts" className="NavLink"> 
+                        <li className='NavItem Cursor'>КОНТАКТЫ</li>
+                    </Link>
                 </ul>
             </nav>
-        </nav >
+        </nav>
     )
 }
 
