9afceda0c72e993605bdb7fc2a508d8e14c3f66b
Artem Ryklin <sylees@yandex.ru>
2025-07-06 21:54:14 +0300

    123

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter} from 'react-router-dom';
 import '../src/styles/index.css';
 import App from './App';
 import Nav from './pages/nav/nav'
-import Footer from './pages/footer/footer.jsx'
+// import Footer from './pages/footer/footer.jsx'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,7 +17,7 @@ root.render(
           <App />
 
       </div>
-      {/* <Footer /> */}
+  
     </div>
   </BrowserRouter>
 );
